35d52b3d9aeca2c84b65e115a1644e79824676cb
maxence.nho <maxence.nho@hetic.net>
2024-06-27 16:11:41 +0200

    Remaniement / Pris en compte des valeurs en fonction de 5 (inférieur / supérieur) et cas particulier

--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -2,16 +2,20 @@ package com.example;
 
 public class Main {
     public static String convertNumberToRoman(int number) {
+        StringBuilder roman = new StringBuilder();
         if(number == 4){
             return "IV";
+        }else if(number < 5){
+            for (int i = 0; i < number; i++) {
+                roman.append("I");
+            }
         }else if(number == 5){
             return "V";
-        }else if(number == 6){
-            return "VI";
-        }
-        StringBuilder roman = new StringBuilder();
-        for (int i = 0; i < number; i++) {
-            roman.append("I");
+        }else{
+            roman.append("V");
+            for (int i = 5; i < number; i++) {
+                roman.append("I");
+            }
         }
         
         return roman.toString();
