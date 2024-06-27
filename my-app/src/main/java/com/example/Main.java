d3241ec1b083696f1994bd112c8e98d84b4cd86b
maxence.nho <maxence.nho@hetic.net>
2024-06-27 15:14:04 +0200

    Ajout de la fonction convertRomanNumeral / Cas I --> 1

--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -1,4 +1,14 @@
 package com.example;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(convertRomanNumeral("I"));
+    }
+
+    public static String convertRomanNumeral(String numeral) {
+        if ("I".equals(numeral)) {
+            return "1";
+        }
+        return "";
+    }
 }
