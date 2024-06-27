6279a8cd90de44f19db57a4b55915bed7ed57a16
maxence.nho <maxence.nho@hetic.net>
2024-06-27 15:56:34 +0200

    Création test 2 --> II / expected: "II"  but was: "I"

--- my-app/src/test/java/com/example/MainTest.java
+++ my-app/src/test/java/com/example/MainTest.java
@@ -6,8 +6,14 @@ import static org.assertj.core.api.Assertions.assertThat;
     public class MainTest {
         
         @Test
-        public void I_should_return_1() {
+        public void Function_1_should_return_II() {
             String result = Main.convertNumberToRoman(1);
             assertThat(result).isEqualTo("I");
         }
+
+        @Test
+        public void Function_2_should_return_II() {
+            String result = Main.convertNumberToRoman(2);
+            assertThat(result).isEqualTo("II");
+        }   
 }
