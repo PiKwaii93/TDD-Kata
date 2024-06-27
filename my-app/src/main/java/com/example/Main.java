d3ec98f5273a3549582c395a3d5bac6404ee352a
maxence.nho <maxence.nho@hetic.net>
2024-06-27 16:06:12 +0200

    Ajout du cas 6 --> VI

--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -6,6 +6,8 @@ public class Main {
             return "IV";
         }else if(number == 5){
             return "V";
+        }else if(number == 6){
+            return "VI";
         }
         StringBuilder roman = new StringBuilder();
         for (int i = 0; i < number; i++) {
