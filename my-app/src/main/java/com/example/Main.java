663f09c8c04bb76cf97c674c191425d1b225bc2b
maxence.nho <maxence.nho@hetic.net>
2024-06-27 16:04:47 +0200

    Ajout du cas 5 --> V

--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static String convertNumberToRoman(int number) {
         if(number == 4){
             return "IV";
+        }else if(number == 5){
+            return "V";
         }
         StringBuilder roman = new StringBuilder();
         for (int i = 0; i < number; i++) {
