3b75c11dea9146dc34e9d8cfdf55816bba469d1a
maxence.nho <maxence.nho@hetic.net>
2024-06-27 16:03:39 +0200

    Ajout du cas 4 --> IV

--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -2,6 +2,9 @@ package com.example;
 
 public class Main {
     public static String convertNumberToRoman(int number) {
+        if(number == 4){
+            return "IV";
+        }
         StringBuilder roman = new StringBuilder();
         for (int i = 0; i < number; i++) {
             roman.append("I");
