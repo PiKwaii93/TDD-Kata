12b1b72d2f49026fdbfbe79ce1a67ec5b5d60c92
maxence.nho <maxence.nho@hetic.net>
2024-06-27 16:14:09 +0200

    Ajout du cas 9 --> IX

--- my-app/src/main/java/com/example/Main.java
+++ my-app/src/main/java/com/example/Main.java
@@ -3,7 +3,9 @@ package com.example;
 public class Main {
     public static String convertNumberToRoman(int number) {
         StringBuilder roman = new StringBuilder();
-        if(number == 4){
+        if(number == 9){
+            return "IX";
+        }else if(number == 4){
             return "IV";
         }else if(number < 5){
             for (int i = 0; i < number; i++) {
